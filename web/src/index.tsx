a03189615d7de80a3f4afd821e735cac445080fa
noahGms <noah@keshin.net>
2024-02-25 03:43:19 +0100

    Feature: Add authentication, stream key management, streamers listing and player on web app

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:8000";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,97 @@
+import {Sidebar, Spinner} from "flowbite-react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import ReactPlayer from "react-player";
+
 export default function Home() {
+  const [streamersLoading, setStreamersLoading] = useState(false)
+  const [streamers, setStreamers] = useState([])
+
+  const [selectedStreamer, setSelectedStreamer] = useState("")
+
+  const fetchStreamers = async () => {
+    setStreamersLoading(true)
+    try {
+      const response = await axios.get('/streams')
+      setStreamers(response.data.online_streamer)
+    } catch (error) {
+      console.log(error)
+    }
+    setStreamersLoading(false)
+  }
+
+  const selectedStreamerUrl = () => {
+    if (!selectedStreamer) return ""
+
+    return `http://localhost:8000/hls/${selectedStreamer}/playlist.m3u8`
+  }
+
+  useEffect(() => {
+    fetchStreamers()
+  }, [])
+
+
+  if (streamersLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Spinner/>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Welcome on DecentralStream
-      </h1>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="flex max-w-screen-xl px-4 py-8 mx-auto gap-8">
+          <div className={'w-1/4'}>
+            <Sidebar className={'w-full'}>
+              <Sidebar.ItemGroup>
+                <h1 className={'text-lg text-center font-medium'}>Active streamers</h1>
+              </Sidebar.ItemGroup>
+              <Sidebar.ItemGroup>
+                {!streamers.length && (
+                  <p className={'text-center text-gray-500 dark:text-gray-400'}>No streamers online</p>
+                )}
+
+                {streamers.map((streamer: string, idx: number) => (
+                  <Sidebar.Items key={idx}>
+                    <Sidebar.ItemGroup>
+                      <Sidebar.Item className={'cursor-pointer'} active={streamer === selectedStreamer}
+                                    onClick={() => setSelectedStreamer(streamer)}>
+                        <span className={'break-all text-wrap'}>
+                          {streamer}
+                        </span>
+                      </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                  </Sidebar.Items>
+                ))}
+              </Sidebar.ItemGroup>
+            </Sidebar>
+          </div>
+          <div className={'w-3/4'}>
+            <div className="lg:col-span-12">
+              {!selectedStreamer ? (
+                <p className={'text-center text-gray-500 dark:text-gray-400'}>Select a streamer to watch</p>
+              ) : (
+                <>
+                  <h1 className={'text-center text-lg font-medium'}>Watching {selectedStreamer}</h1>
+
+                  <ReactPlayer
+                    url={selectedStreamerUrl()}
+                    options={{
+                      forceHLS: true
+                    }}
+                    controls
+                    width="100%"
+                    height="100%"
+                    className="mt-4"
+                  />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
