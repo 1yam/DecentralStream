6bed2227714d4a394def8b0a90c46096f6e56836
noahGms <noah@keshin.net>
2024-02-25 12:19:53 +0100

    Feature: Use hookstate to manage account state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,11 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import {Header} from "./components/Header";
 import Web3 from "web3";
+import {useAccountState} from "./stores/account";
 
 function App() {
   const Layout = ({children}: { children: React.ReactNode }) => {
+    const accountState = useAccountState();
     const [authLoading, setAuthLoading] = useState(false);
     const [address, setAddress] = useState("");
 
@@ -21,6 +23,7 @@ function App() {
 
           const account = Web3.utils.toChecksumAddress(accounts[0]);
           setAddress(account);
+          accountState.setAddress(account);
         }
       } catch (error) {
         console.log(error);
@@ -29,7 +32,10 @@ function App() {
       setAuthLoading(false);
     };
 
-    const logout = () => setAddress("");
+    const logout = () => {
+      setAddress("");
+      accountState.setAddress("");
+    }
 
     return (
       <div className="flex flex-col h-screen">
